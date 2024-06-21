f5387311cc0302c1243a355a3bb1a53d6dc4c595
emmadev <macanthonyemmanuel12@gmail.com>
2024-06-21 13:45:20 +0100

    finished: basics

--- basics/App.js
+++ basics/App.js
@@ -3,22 +3,35 @@ import Animated, {
   withTiming,
   useAnimatedStyle,
   Easing,
+  withSpring,
+  withRepeat,
 } from "react-native-reanimated";
-import { View, Button } from "react-native";
+import { View, Button, Text } from "react-native";
+import { useEffect } from "react";
+
+const SIZE = 100;
 
 export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  const handleRotation = (progress) => {
+    "worklet";
+    return `${progress.value * 2 * Math.PI}rad`;
   };
 
-  const style = useAnimatedStyle(() => {
+  const reanimatedStyle = useAnimatedStyle(() => {
     return {
-      width: withTiming(randomWidth.value, config),
+      opacity: progress.value,
+      transform: [{ scale: scale.value }, { rotate: handleRotation(progress) }],
+      borderRadius: (progress.value * SIZE) / 2,
     };
-  });
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), 3, true);
+    scale.value = withRepeat(withSpring(1), 3, true);
+  }, []);
 
   return (
     <View
@@ -31,16 +44,14 @@ export default function AnimatedStyleUpdateExample(props) {
     >
       <Animated.View
         style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
+          {
+            height: SIZE,
+            width: SIZE,
+            backgroundColor: "blue",
+          },
+          reanimatedStyle,
         ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+      ></Animated.View>
     </View>
   );
 }
