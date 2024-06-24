37ead4f266c88d59cb3de59c0b1e1626c82a99f1
emmadev <macanthonyemmanuel12@gmail.com>
2024-06-24 07:15:29 +0100

    finished:pinch-gesture

---
+++ pinch-gesture/App.tsx
@@ -0,0 +1,92 @@
+import { StatusBar } from "expo-status-bar";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import {
+  GestureHandlerRootView,
+  PinchGestureHandler,
+  PinchGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+
+const IMAGE_URI =
+  "https://images.unsplash.com/photo-1621569642780-4864752e847e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+const { width, height } = Dimensions.get("screen");
+export default function App() {
+  const scale = useSharedValue(1);
+  const focalX = useSharedValue(0);
+  const focalY = useSharedValue(0);
+
+  const pinchHandler =
+    useAnimatedGestureHandler<PinchGestureHandlerGestureEvent>({
+      onActive: (event) => {
+        scale.value = event.scale;
+        focalX.value = event.focalX;
+        focalY.value = event.focalY;
+      },
+      onEnd: (event) => {
+        scale.value = withTiming(1);
+      },
+    });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: focalX.value },
+        { translateY: focalY.value },
+        { translateX: -width / 2 },
+        { translateY: -height / 2 },
+        { scale: scale.value },
+        { translateX: -focalX.value },
+        { translateY: -focalY.value },
+        { translateX: width / 2 },
+        { translateY: height / 2 },
+      ],
+    };
+  });
+
+  const focalPointStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: focalX.value }, { translateY: focalY.value }],
+    };
+  });
+  return (
+    <GestureHandlerRootView>
+      <PinchGestureHandler onGestureEvent={pinchHandler}>
+        <Animated.View style={{ flex: 1 }}>
+          <AnimatedImage
+            style={[{ flex: 1 }, rStyle]}
+            source={{ uri: IMAGE_URI }}
+          />
+          <Animated.View style={[styles.focal, focalPointStyle]} />
+        </Animated.View>
+      </PinchGestureHandler>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  focal: {
+    width: 20,
+    height: 20,
+    backgroundColor: "blue",
+    borderRadius: 10,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
